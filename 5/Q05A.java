756e233ba5781c7db53e9d4b01fd8d12e47ec86f
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-05-18 22:29:56 -0300

    Create a class to do Synchronized Map operations

--- 5/Q05A.java
+++ 5/Q05A.java
@@ -47,14 +47,75 @@ class DoCHMOperations implements Runnable {
         for (int i = 0; i < maxOp; i++) {
             if (inserCount < this.totalInsertOperations) {
                 this.cmap.put(this.insertKeys[i], this.insertValues[i]);
+                inserCount++;
             }
             
             if (deleteCount < this.totalDeleteOperations) {
                 this.cmap.remove(this.deleteKeys[i]);
+                deleteCount++;
             }
 
             if (readCount < this.totalReadOperations) {
                 this.cmap.get(getKeys[i]);
+                readCount++;
+            }
+        }
+    }
+}
+
+
+class DoSMOperations implements Runnable {
+    Map smap;
+    int totalInsertOperations;
+    int totalDeleteOperations;
+    int totalReadOperations;
+    
+    Integer insertKeys[];
+    Integer insertValues[];
+    Integer deleteKeys[];
+    Integer getKeys[];
+
+
+    public DoSMOperations(Map smapInstance,
+                         int totalInsertOperations,
+                         int totalDeleteOperations,
+                         int totalReadOperations,
+                         Integer insertKeys[],
+                         Integer insertValues[],
+                         Integer deleteKeys[],
+                         Integer getKeys[]) {
+        this.smap = smapInstance;
+        this.totalInsertOperations = totalInsertOperations;
+        this.totalDeleteOperations = totalDeleteOperations;
+        this.totalReadOperations = totalReadOperations;
+        this.insertKeys = insertKeys;
+        this.deleteKeys = deleteKeys;
+        this.getKeys = getKeys;
+        this.insertValues = insertValues;
+    }
+
+    public void run() {
+        int maxOp = Math.max(this.totalInsertOperations,
+                             Math.max(this.totalDeleteOperations, this.totalReadOperations));
+        
+        int inserCount = 0;
+        int deleteCount = 0;
+        int readCount = 0;
+
+        for (int i = 0; i < maxOp; i++) {
+            if (inserCount < this.totalInsertOperations) {
+                this.smap.put(this.insertKeys[i], this.insertValues[i]);
+                inserCount++;
+            }
+            
+            if (deleteCount < this.totalDeleteOperations) {
+                this.smap.remove(this.deleteKeys[i]);
+                deleteCount++;
+            }
+
+            if (readCount < this.totalReadOperations) {
+                this.smap.get(getKeys[i]);
+                readCount++;
             }
         }
     }
