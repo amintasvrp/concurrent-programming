668b220736cdd9e923d2a9344aaeabc4372a5d45
avdLuna <lunaft9@gmail.com>
2019-05-20 19:10:35 -0300

    Sequencial threads code and run

---
+++ 1/ThreadsSequenciais.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+#include <pthread.h>
+#include <mutex>
+
+using namespace std;
+
+long int counter = 0;
+mutex mtx;
+
+void* run(void* args){
+    long int my_id;
+    long int j;
+
+    my_id = (long int) args;
+
+    mtx.lock();
+    for (j = 0; j < 1e7; j++) {
+        counter++;
+    }
+    mtx.unlock();
+
+    printf("my_id= %ld j=%ld counter=%ld\n", my_id,j, counter);
+}
+
+int main(int argc, char *argv[]){
+    int i;
+    pthread_t pthreads[3];
+
+    for (i = 0; i < 3; i++) {
+        pthread_create(&pthreads[i], NULL, &run, (void*) i);
+    }
+
+    for (i = 0; i < 3; i++) {
+        pthread_join(pthreads[i], NULL);
+    }
+
+    return 0;
+}
